aca21d8289ae20f256696abda491a985104144b8
Simon Woolf <simon@ably.io>
2017-05-05 23:31:56 +0100

    Force dep versions to allow riak to compile

--- mix.exs
+++ mix.exs
@@ -19,6 +19,10 @@ defmodule AblyNg.Mixfile do
       {:riak_core, "~> 3.0", hex: :riak_core_ng},
       {:cowboy, github: "ninenines/cowboy", tag: "2.0.0-pre.8"},
       {:poison, "~> 3.1"},
+      # nb see https://github.com/project-fifo/riak_core/issues/20
+      {:cuttlefish, github: "basho/cuttlefish", tag: "2.0.11", manager: :rebar3, override: true},
+      {:lager, github: "basho/lager", tag: "3.2.4", manager: :rebar3, override: true},
+      {:goldrush, github: "basho/goldrush", tag: "0.1.9", manager: :rebar3, override: true}
     ]
   end
 end
